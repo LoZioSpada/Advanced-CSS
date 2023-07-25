174199d6526e4b04ebb1733e8e4f76245cc7bae0
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-07-25 20:29:16 +0200

    Advanced CSS
    - Effettuato gli esercizi EXTRA su JavaScript all'interno del file script.js;

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
-// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+window.onload = function(){
+    document.querySelector(".blog-sidebar div:last-child li:nth-of-type(2)").remove();
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".   
+     
+    for(let link of document.querySelectorAll('.stretched-link')){
+        link.onclick = function(event){
+            event.preventDefault()
+            event.target.closest('.col-md-6').remove();
+        }
+    }
+
+    for(let link2 of document.querySelectorAll('.font-weight-bold')){
+        link2.onclick = function(event) {
+            event.preventDefault();
+            event.target.closest('.px-0').remove();
+        }
+    }
 // EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
+    for(let author of document.querySelectorAll('.blog-post-meta a')){
+        author.addEventListener('mouseover', function(event){
+            alert(event.target.innerText);
+        });
+    }
+}
+
+
+//appunto: parent container: document.querySelector('#btn').parentElement
+// document.querySelector('#btn').closest per esercizio 2
